7dcf555848351edcaa04efe25540c8f52ec55744
PuntaRollers <claudiofaccelli@gmail.com>
2026-05-01 22:38:39 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './styles/global.css'
 
-import App        from './App'
-import AdminPanel from './components/Admin/AdminPanel'
+import App            from './App'
+import AdminPanel     from './components/Admin/AdminPanel'
+import LocationDetail from './components/Detail/LocationDetail'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/"      element={<App />} />
-        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/"          element={<App />} />
+        <Route path="/admin"     element={<AdminPanel />} />
+        <Route path="/lugar/:slug" element={<LocationDetail />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
